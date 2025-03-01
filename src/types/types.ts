7d54c3b79fcefbb510721579b16aa9f90b711822
Nirjal Prajapati <34954284+nirjalpraj@users.noreply.github.com>
2025-03-01 13:19:11 +0545

    chabnges

---
+++ src/types/types.ts
@@ -0,0 +1,9 @@
+export interface Project {
+    title: string;
+    position: "left" | "right";
+    githublink: string;
+    websitelink: string;
+    photoUrl: string;
+    description: string[];
+    TechStack: string[];
+  }
